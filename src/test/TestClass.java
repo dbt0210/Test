bbd1c74a7ebac03a8b8a6e30fe87d242377f79f5
dbt0210 <dbt0210@hotmail.de>
2014-10-30 13:09:42 +0100

    Initial Project

---
+++ src/test/TestClass.java
@@ -0,0 +1,14 @@
+package test;
+
+public class TestClass {
+
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
