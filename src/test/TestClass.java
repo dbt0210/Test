cc861add677843310758676b8ba3fded0c0f2b9c
dbt0210 <dbt0210@hotmail.de>
2014-11-06 11:03:41 +0100

    bla test

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -2,13 +2,10 @@ package test;
 
 public class TestClass {
 
-	public TestClass() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("This is a test.");
+		//bla
 	}
 
 }
